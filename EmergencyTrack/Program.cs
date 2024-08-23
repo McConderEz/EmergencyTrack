1012eca65f1e5f871e39f1307cd5ce5b46d0ae49
McConderEz <kamazitun.jaykehd@gmail.com>
2024-08-23 20:38:14 +0300

    Добавьте файлы проекта.

---
+++ EmergencyTrack/Program.cs
@@ -0,0 +1,25 @@
+using EmergencyTrack.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
